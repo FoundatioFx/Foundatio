815d92611903ea658da08732e09fe9356a0ed483
Eric J. Smith <eric@codesmithtools.com>
2015-02-19 21:09:59 -0600

    Fixed warnings.

--- src/Core/ServiceProvider/IBootstrappedServiceProvider.cs
+++ src/Core/ServiceProvider/IBootstrappedServiceProvider.cs
@@ -3,6 +3,5 @@ using System;
 namespace Foundatio.ServiceProvider {
     public interface IBootstrappedServiceProvider : IServiceProvider {
         IServiceProvider Bootstrap();
-        object GetService(Type serviceType);
     }
 }
