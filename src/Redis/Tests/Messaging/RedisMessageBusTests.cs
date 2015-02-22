33dc4955c6597ad3eb71f5b6c85073e05f073a35
Eric J. Smith <eric@codesmithtools.com>
2015-02-22 01:37:35 -0600

    Only run 1 test to see if it breaks by itself.

--- src/Redis/Tests/Messaging/RedisMessageBusTests.cs
+++ src/Redis/Tests/Messaging/RedisMessageBusTests.cs
@@ -21,39 +21,39 @@ namespace Foundatio.Redis.Tests.Messaging {
             base.CanSendMessage();
         }
 
-        [Fact]
-        public override void CanSendDelayedMessage() {
-            base.CanSendDelayedMessage();
-        }
+        //[Fact]
+        //public override void CanSendDelayedMessage() {
+        //    base.CanSendDelayedMessage();
+        //}
 
-        [Fact]
-        public override void CanSendMessageToMultipleSubscribers() {
-            base.CanSendMessageToMultipleSubscribers();
-        }
+        //[Fact]
+        //public override void CanSendMessageToMultipleSubscribers() {
+        //    base.CanSendMessageToMultipleSubscribers();
+        //}
 
-        [Fact]
-        public override void CanTolerateSubscriberFailure() {
-            base.CanTolerateSubscriberFailure();
-        }
+        //[Fact]
+        //public override void CanTolerateSubscriberFailure() {
+        //    base.CanTolerateSubscriberFailure();
+        //}
 
-        [Fact]
-        public override void WillOnlyReceiveSubscribedMessageType() {
-            base.WillOnlyReceiveSubscribedMessageType();
-        }
+        //[Fact]
+        //public override void WillOnlyReceiveSubscribedMessageType() {
+        //    base.WillOnlyReceiveSubscribedMessageType();
+        //}
 
-        [Fact]
-        public override void WillReceiveDerivedMessageTypes() {
-            base.WillReceiveDerivedMessageTypes();
-        }
+        //[Fact]
+        //public override void WillReceiveDerivedMessageTypes() {
+        //    base.WillReceiveDerivedMessageTypes();
+        //}
 
-        [Fact]
-        public override void CanSubscribeToAllMessageTypes() {
-            base.CanSubscribeToAllMessageTypes();
-        }
+        //[Fact]
+        //public override void CanSubscribeToAllMessageTypes() {
+        //    base.CanSubscribeToAllMessageTypes();
+        //}
 
-        [Fact]
-        public override void WontKeepMessagesWithNoSubscribers() {
-            base.WontKeepMessagesWithNoSubscribers();
-        }
+        //[Fact]
+        //public override void WontKeepMessagesWithNoSubscribers() {
+        //    base.WontKeepMessagesWithNoSubscribers();
+        //}
     }
 }
