4030a6ac23b44f71eeb98f3cbf596c3be22f2cd2
Eric J. Smith <eric@codesmithtools.com>
2015-02-28 16:43:24 -0600

    Update to xunit 2.0

--- src/Redis/Tests/Properties/AssemblyInfo.cs
+++ src/Redis/Tests/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: Xunit.CollectionBehaviorAttribute(DisableTestParallelism = true)]
+[assembly: Xunit.CollectionBehaviorAttribute(DisableTestParallelization = true)]
