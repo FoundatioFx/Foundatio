06d7d6180243ca4f6a998950cf6d2efca47ff256
Blake Niemyjski <bniemyjski@gmail.com>
2015-04-01 11:09:20 -0500

    Added performance metrics to the unit tests as well as a multithreaded test

--- src/Core/Tests/Metrics/StatsDMetricsTests.cs
+++ src/Core/Tests/Metrics/StatsDMetricsTests.cs
@@ -7,17 +7,21 @@ using System.Threading.Tasks;
 using Foundatio.Metrics;
 using Foundatio.Tests.Utility;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Foundatio.Tests.Metrics {
     public class StatsDMetricsTests : IDisposable {
+        private readonly TestOutputWriter _writer;
         private readonly int _port = new Random(12345).Next(10000, 15000);
         private readonly StatsDMetricsClient _client;
         private readonly UdpListener _listener;
         private Thread _listenerThread;
 
-        public StatsDMetricsTests() {
+        public StatsDMetricsTests(ITestOutputHelper output) {
             _listener = new UdpListener("127.0.0.1", _port);
             _client = new StatsDMetricsClient("127.0.0.1", _port, "test");
+
+            _writer = new TestOutputWriter(output);
         }
 
         [Fact]
@@ -68,18 +72,35 @@ namespace Foundatio.Tests.Metrics {
             const int iterations = 10000;
             StartListening(iterations);
 
+            var metrics = new InMemoryMetricsClient();
             var sw = new Stopwatch();
             sw.Start();
-            for (int index = 0; index < iterations; index++)
+            for (int index = 0; index < iterations; index++) {
                 await _client.CounterAsync("counter");
+                await metrics.CounterAsync("counter");
+            }
 
             sw.Stop();
+            metrics.DisplayStats(_writer);
             Assert.InRange(sw.ElapsedMilliseconds, 0, 450);
             
             var messages = GetMessages();
             Assert.Equal(iterations, messages.Count);
             for (int index = 0; index < iterations; index++)
                 Assert.Equal("test.counter:1|c", messages[index]);
+        }
+
+        [Fact]
+        public void CanSendMultithreaded() {
+            const int iterations = 100;
+            StartListening(iterations);
+            Parallel.For(0, iterations, async i => {
+                await _client.CounterAsync("counter");
+                Thread.Sleep(50);
+            });
+
+            var messages = GetMessages();
+            Assert.Equal(iterations, messages.Count);
         }
 
         private List<string> GetMessages() {
