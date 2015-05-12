11cb1bb24658597d71b65f98734a838e527225bd
Blake Niemyjski <bniemyjski@gmail.com>
2015-05-11 19:48:32 -0500

    Moved the metrics.net sample into the src folder.
    @ejsmith do you like the nuget package name? I'm wondering if we should
    put it in a folder called Metrics.NET and a package
    Foundatio.Metrics.NET? Thoughts?

---
+++ src/MetricsNet/MetricsNetClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Foundatio.Utility;
+using Metrics;
+
+namespace Foundatio.Metrics {
+    public class MetricsNetClient : IMetricsClient {
+        public Task CounterAsync(string statName, int value = 1) {
+            Metric.Counter(statName, Unit.None).Increment();
+            return TaskHelper.Completed();
+        }
+
+        public Task GaugeAsync(string statName, double value) {
+            Metric.Gauge(statName, () => value, Unit.None);
+            return TaskHelper.Completed();
+        }
+
+        public Task TimerAsync(string statName, long milliseconds) {
+            Metric.Timer(statName, Unit.Calls, SamplingType.SlidingWindow, TimeUnit.Milliseconds).Record(milliseconds, TimeUnit.Milliseconds);
+            return TaskHelper.Completed();
+        }
+
+        public void Dispose() {}
+    }
+}
