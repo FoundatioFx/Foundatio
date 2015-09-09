e1af37e88613dfc08b9eb8727f926a3686bd8cc5
Blake Niemyjski <bniemyjski@gmail.com>
2015-09-09 00:29:48 -0500

    Fixed a deadlock with the message bus

--- src/Core/Messaging/MessageBusBase.cs
+++ src/Core/Messaging/MessageBusBase.cs
@@ -1,15 +1,15 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Foundatio.Extensions;
 using Foundatio.Logging;
-using Nito.AsyncEx;
+using Foundatio.Utility;
 
 namespace Foundatio.Messaging {
     public abstract class MessageBusBase : IMessagePublisher, IDisposable {
-        private readonly AsyncLock _asyncLock = new AsyncLock();
-		private readonly List<DelayedMessage> _delayedMessages = new List<DelayedMessage>();
+		private readonly ConcurrentDictionary<Guid, DelayedMessage> _delayedMessages = new ConcurrentDictionary<Guid, DelayedMessage>();
         private DateTime? _nextMaintenance = null;
         private readonly Timer _maintenanceTimer;
 
@@ -19,69 +19,68 @@ namespace Foundatio.Messaging {
 
         public abstract Task PublishAsync(Type messageType, object message, TimeSpan? delay = null, CancellationToken cancellationToken = default(CancellationToken));
 
-        protected async Task AddDelayedMessageAsync(Type messageType, object message, TimeSpan delay) {
+        protected Task AddDelayedMessageAsync(Type messageType, object message, TimeSpan delay) {
             if (message == null)
-                throw new ArgumentNullException("message");
+                throw new ArgumentNullException(nameof(message));
+            
+            var sendTime = DateTime.UtcNow.Add(delay);
+            _delayedMessages.TryAdd(Guid.NewGuid(), new DelayedMessage {
+                Message = message,
+                MessageType = messageType,
+                SendTime = sendTime
+            });
 
-            using (await _asyncLock.LockAsync()) {
-                var sendTime = DateTime.UtcNow.Add(delay);
-                _delayedMessages.Add(new DelayedMessage {
-                    Message = message,
-                    MessageType = messageType,
-                    SendTime = sendTime
-                });
+            ScheduleNextMaintenance(sendTime);
 
-                await ScheduleNextMaintenance(sendTime).AnyContext();
-            }
+            return TaskHelper.Completed();
         }
 
-        private async Task ScheduleNextMaintenance(DateTime value) {
+        private void ScheduleNextMaintenance(DateTime value) {
             Logger.Trace().Message("ScheduleNextMaintenance: value={0}", value).Write();
             if (value == DateTime.MaxValue)
                 return;
+            
+            if (_nextMaintenance.HasValue && _nextMaintenance.Value < DateTime.UtcNow)
+                _nextMaintenance = null;
 
-            using (await _asyncLock.LockAsync()) {
-                if (_nextMaintenance.HasValue && _nextMaintenance.Value < DateTime.UtcNow)
-                    _nextMaintenance = null;
+            if (_nextMaintenance.HasValue && value > _nextMaintenance.Value)
+                return;
 
-                if (_nextMaintenance.HasValue && value > _nextMaintenance.Value)
-                    return;
-
-                int delay = Math.Max((int)value.Subtract(DateTime.UtcNow).TotalMilliseconds, 0);
-                _nextMaintenance = value;
-                Logger.Trace().Message("Scheduling delayed task: delay={0}", delay).Write();
-                _maintenanceTimer.Change(delay, Timeout.Infinite);
-            }
+            int delay = Math.Max((int)value.Subtract(DateTime.UtcNow).TotalMilliseconds, 0);
+            _nextMaintenance = value;
+            Logger.Trace().Message("Scheduling delayed task: delay={0}", delay).Write();
+            _maintenanceTimer.Change(delay, Timeout.Infinite);
         }
 
         private async Task DoMaintenanceAsync() {
-            using (await _asyncLock.LockAsync()) {
-	            Logger.Trace().Message("DoMaintenanceAsync").Write();
-                if (_delayedMessages == null || _delayedMessages.Count == 0)
-                    return;
+	        Logger.Trace().Message("DoMaintenanceAsync").Write();
+            if (_delayedMessages == null || _delayedMessages.Count == 0)
+                return;
 
-                DateTime nextMessageSendTime = DateTime.MaxValue;
-                var now = DateTime.UtcNow;
-                var messagesToSend = new List<DelayedMessage>();
+            DateTime nextMessageSendTime = DateTime.MaxValue;
+            var now = DateTime.UtcNow;
+            var messagesToSend = new List<Guid>();
 
-                foreach (var message in _delayedMessages) {
-                    if (message.SendTime <= now)
-                        messagesToSend.Add(message);
-                    else if (message.SendTime < nextMessageSendTime)
-                        nextMessageSendTime = message.SendTime;
-                }
+            foreach (var pair in _delayedMessages) {
+                if (pair.Value.SendTime <= now)
+                    messagesToSend.Add(pair.Key);
+                else if (pair.Value.SendTime < nextMessageSendTime)
+                    nextMessageSendTime = pair.Value.SendTime;
+            }
 
-                _nextMaintenance = null;
-                await ScheduleNextMaintenance(nextMessageSendTime).AnyContext();
+            _nextMaintenance = null;
+            ScheduleNextMaintenance(nextMessageSendTime);
 
-                if (messagesToSend.Count == 0)
-                    return;
+            if (messagesToSend.Count == 0)
+                return;
 
-                foreach (var message in messagesToSend) {
-                    Logger.Trace().Message("DoMaintenance Send Delayed: {0}", message.MessageType).Write();
-                    _delayedMessages.Remove(message);
-                    await PublishAsync(message.MessageType, message.Message).AnyContext();
-                }
+            foreach (var messageId in messagesToSend) {
+                DelayedMessage message;
+                if (!_delayedMessages.TryRemove(messageId, out message))
+                    continue;
+
+                Logger.Trace().Message("DoMaintenance Send Delayed: {0}", message.MessageType).Write();
+                await PublishAsync(message.MessageType, message.Message).AnyContext();
             }
         }
 
