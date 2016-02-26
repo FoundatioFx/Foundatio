acd46eced629767eee30fecbdb427c007827816c
ejsmith <eric@codesmithtools.com>
2016-02-26 14:23:54 -0600

    Playing around with BenchmarkDotNet

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Benchmarks.Queues;
+
+namespace Foundatio.Benchmarks {
+    class Program {
+        static void Main(string[] args) {
+            var summary = BenchmarkRunner.Run<QueueBenchmarks>(new BenchmarkConfig());
+        }
+    }
+
+    class BenchmarkConfig : ManualConfig {
+        public BenchmarkConfig() {
+            Add(Job.Default.With(Mode.SingleRun).WithWarmupCount(1).WithTargetCount(1));
+        }
+    }
+}
---
+++ src/Benchmarks/Queues/QueueBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Foundatio.Queues;
+using StackExchange.Redis;
+
+namespace Foundatio.Benchmarks.Queues {
+    public class QueueBenchmarks {
+        private const int _itemCount = 1000;
+
+        [Benchmark]
+        public void ProcessInMemoryQueue() {
+            var _queue = new InMemoryQueue<QueueItem>();
+
+            for (int i = 0; i < _itemCount; i++)
+                _queue.EnqueueAsync(new QueueItem { Id = i }).GetAwaiter().GetResult();
+
+            for (int i = 0; i < _itemCount; i++)
+                _queue.DequeueAsync(TimeSpan.Zero).GetAwaiter().GetResult();
+        }
+
+        [Benchmark]
+        public void ProcessRedisQueue() {
+            var muxer = ConnectionMultiplexer.Connect("localhost");
+            var _queue = new RedisQueue<QueueItem>(muxer);
+
+            for (int i = 0; i < _itemCount; i++)
+                _queue.EnqueueAsync(new QueueItem { Id = i }).GetAwaiter().GetResult();
+
+            for (int i = 0; i < _itemCount; i++)
+                _queue.DequeueAsync(TimeSpan.Zero).GetAwaiter().GetResult();
+        }
+    }
+
+    public class QueueItem {
+        public int Id { get; set; }
+    }
+}
