8cac3988086ee4211f874842da21824a13e714fc
ejsmith <eric@codesmithtools.com>
2016-03-01 16:35:21 -0600

    Add ability to get queue stats.

--- src/Core/Metrics/IMetricsClientStats.cs
+++ src/Core/Metrics/IMetricsClientStats.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Foundatio.Extensions;
+using Foundatio.Queues;
 
 namespace Foundatio.Metrics {
     public interface IMetricsClientStats {
@@ -18,6 +19,28 @@ namespace Foundatio.Metrics {
         public static async Task<double> GetLastGaugeValueAsync(this IMetricsClientStats stats, string name, DateTime? start = null, DateTime? end = null) {
             var result = await stats.GetGaugeStatsAsync(name, start, end).AnyContext();
             return result.Last;
+        }
+
+        public static async Task<QueueStatSummary> GetQueueStatsAsync(this IMetricsClientStats stats, string name, DateTime? start = null, DateTime? end = null) {
+            var countTask = stats.GetGaugeStatsAsync($"{name}.counter", start, end);
+            var enqueuedTask = stats.GetCounterStatsAsync($"{name}.enqueued", start, end);
+            var queueTimeTask = stats.GetTimerStatsAsync($"{name}.queuetime", start, end);
+            var dequeuedTask = stats.GetCounterStatsAsync($"{name}.dequeued", start, end);
+            var completedTask = stats.GetCounterStatsAsync($"{name}.completed", start, end);
+            var abandonedTask = stats.GetCounterStatsAsync($"{name}.abandoned", start, end);
+            var processTimeTask = stats.GetTimerStatsAsync($"{name}.processtime", start, end);
+
+            await Task.WhenAll(countTask, enqueuedTask, queueTimeTask, dequeuedTask, completedTask, abandonedTask, processTimeTask);
+
+            return new QueueStatSummary {
+                Count = countTask.Result,
+                Enqueued = enqueuedTask.Result,
+                QueueTime = queueTimeTask.Result,
+                Dequeued = dequeuedTask.Result,
+                Completed = completedTask.Result,
+                Abandoned = abandonedTask.Result,
+                ProcessTime = processTimeTask.Result
+            };
         }
     }
 }
---
+++ src/Core/Queues/QueueStatSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Foundatio.Metrics;
+
+namespace Foundatio.Queues {
+    public class QueueStatSummary {
+        public GaugeStatSummary Count { get; set; }
+        public CounterStatSummary Enqueued { get; set; }
+        public TimingStatSummary QueueTime { get; set; }
+        public CounterStatSummary Dequeued { get; set; }
+        public CounterStatSummary Completed { get; set; }
+        public CounterStatSummary Abandoned { get; set; }
+        public TimingStatSummary ProcessTime { get; set; }
+    }
+}
