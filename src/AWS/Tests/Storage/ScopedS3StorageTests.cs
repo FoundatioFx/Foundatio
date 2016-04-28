c273e8c7a19fc43a2a68e1db139603c409fa7690
ejsmith <eric@codesmithtools.com>
2016-04-27 23:28:51 -0500

    Add scoped storage

--- src/AWS/Tests/Storage/ScopedS3StorageTests.cs
+++ src/AWS/Tests/Storage/ScopedS3StorageTests.cs
@@ -3,13 +3,12 @@ using System.Threading.Tasks;
 using Amazon;
 using Amazon.Runtime;
 using Foundatio.Storage;
-using Foundatio.Tests.Storage;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Foundatio.AWS.Tests.Storage {
-    public class S3StorageTests : FileStorageTestsBase {
-        public S3StorageTests(ITestOutputHelper output) : base(output) {}
+namespace Foundatio.Tests.Storage {
+    public class ScopedS3StorageTests : FileStorageTestsBase {
+        public ScopedS3StorageTests(ITestOutputHelper output) : base(output) {}
 
         protected override IFileStorage GetStorage() {
             var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
@@ -17,14 +16,14 @@ namespace Foundatio.AWS.Tests.Storage {
             if (String.IsNullOrEmpty(accessKey) || String.IsNullOrEmpty(secretKey))
                 return null;
 
-            return new S3Storage(new BasicAWSCredentials(accessKey, secretKey), RegionEndpoint.USEast1, "foundatio", loggerFactory: Log);
+            return new ScopedFileStorage(new S3Storage(new BasicAWSCredentials(accessKey, secretKey), RegionEndpoint.USEast1, "foundatio", loggerFactory: Log), "scope");
         }
 
         [Fact]
         public override Task CanGetEmptyFileListOnMissingDirectory() {
             return base.CanGetEmptyFileListOnMissingDirectory();
         }
-        
+
         [Fact]
         public override Task CanGetFileListForSingleFolder() {
             return base.CanGetFileListForSingleFolder();
