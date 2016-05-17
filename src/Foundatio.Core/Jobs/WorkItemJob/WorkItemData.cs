bb96f8a4de2fd3624fd19ae18c0688de0510fbc6
bartmax <bart@bartmax.com>
2016-05-16 22:05:16 -0300

    initial changes to adopt project.json programming model.

---
+++ src/Foundatio.Core/Jobs/WorkItemJob/WorkItemData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Foundatio.Metrics;
+
+namespace Foundatio.Jobs {
+    public class WorkItemData : IHaveMetricName {
+        public string WorkItemId { get; set; }
+        public string Type { get; set; }
+        public string Data { get; set; }
+        public bool SendProgressReports { get; set; }
+
+        public string GetMetricName() {
+            if (String.IsNullOrEmpty(Type))
+                return null;
+
+            var type = GetTypeName(Type);
+            if (type != null && type.EndsWith("WorkItem"))
+                type = type.Substring(0, type.Length - 8);
+
+            return type?.ToLowerInvariant();
+        }
+
+        public string GetTypeName(string assemblyQualifiedName) {
+            if (String.IsNullOrEmpty(assemblyQualifiedName))
+                return null;
+
+            string[] parts = assemblyQualifiedName.Split(',');
+            int i = parts[0].LastIndexOf('.');
+            if (i < 0)
+                return null;
+
+            return parts[0].Substring(i + 1);
+        }
+    }
+}
---
+++ src/Foundatio.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Foundatio")]
+[assembly: AssemblyProduct("Foundatio")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Core.Tests")]
