0c85202fc8fe1445bbdc2b75632d23529a0d0a3c
Blake Niemyjski <bniemyjski@gmail.com>
2016-07-01 16:59:44 -0500

    Some updates to the scheduler tests

--- test/Foundatio.Tests/Utility/ScheduledTimerTests.cs
+++ test/Foundatio.Tests/Utility/ScheduledTimerTests.cs
@@ -18,16 +18,17 @@ namespace Foundatio.Tests.Utility {
             Log.SetLogLevel<ScheduledTimer>(LogLevel.Trace);
 
             int hits = 0;
-            var timer = new ScheduledTimer(async () => {
+            Func<Task<DateTime?>> callback = async () => {
                 Interlocked.Increment(ref hits);
                 await Task.Delay(50);
                 return null;
-            }, loggerFactory: Log);
+            };
 
-            timer.ScheduleNext();
-
-            await Task.Delay(50);
-            Assert.Equal(1, hits);
+            using (var timer = new ScheduledTimer(callback, loggerFactory: Log)) {
+                timer.ScheduleNext();
+                await Task.Delay(50);
+                Assert.Equal(1, hits);
+            }
         }
 
         [Fact]
@@ -35,23 +36,25 @@ namespace Foundatio.Tests.Utility {
             Log.SetLogLevel<ScheduledTimer>(LogLevel.Trace);
 
             int hits = 0;
-            var timer = new ScheduledTimer(async () => {
+            Func<Task<DateTime?>> callback = async () => {
                 _logger.Info("Starting work.");
                 Interlocked.Increment(ref hits);
                 await Task.Delay(1000);
                 _logger.Info("Finished work.");
                 return null;
-            }, loggerFactory: Log);
+            };
 
-            timer.ScheduleNext();
-            await Task.Delay(1);
-            timer.ScheduleNext();
+            using (var timer = new ScheduledTimer(callback, loggerFactory: Log)) {
+                timer.ScheduleNext();
+                await Task.Delay(1);
+                timer.ScheduleNext();
 
-            await Task.Delay(50);
-            Assert.Equal(1, hits);
+                await Task.Delay(50);
+                Assert.Equal(1, hits);
 
-            await Task.Delay(1000);
-            Assert.Equal(2, hits);
+                await Task.Delay(1000);
+                Assert.Equal(2, hits);
+            }
         }
 
         [Fact]
@@ -60,56 +63,57 @@ namespace Foundatio.Tests.Utility {
             var resetEvent = new AsyncAutoResetEvent(false);
 
             int hits = 0;
-            var timer = new ScheduledTimer(() => {
+            Func<Task<DateTime?>> callback = () => {
                 Interlocked.Increment(ref hits);
                 resetEvent.Set();
                 return Task.FromResult<DateTime?>(null);
-            }, minimumIntervalTime: TimeSpan.FromMilliseconds(100), loggerFactory: Log);
+            };
 
-            timer.ScheduleNext();
-            await Task.Delay(1);
-            timer.ScheduleNext();
-            await Task.Delay(1);
-            timer.ScheduleNext();
+            using (var timer = new ScheduledTimer(callback, minimumIntervalTime: TimeSpan.FromMilliseconds(100), loggerFactory: Log)) {
+                timer.ScheduleNext();
+                await Task.Delay(1);
+                timer.ScheduleNext();
+                await Task.Delay(1);
+                timer.ScheduleNext();
 
-            await resetEvent.WaitAsync(new CancellationTokenSource(100).Token);
-            var sw = Stopwatch.StartNew();
-            Assert.Equal(1, hits);
+                await resetEvent.WaitAsync(new CancellationTokenSource(100).Token);
+                var sw = Stopwatch.StartNew();
+                Assert.Equal(1, hits);
 
-            await resetEvent.WaitAsync(new CancellationTokenSource(500).Token);
-            sw.Stop();
-            Assert.Equal(2, hits);
+                await resetEvent.WaitAsync(new CancellationTokenSource(500).Token);
+                sw.Stop();
+                Assert.Equal(2, hits);
 
-            Assert.Throws<TaskCanceledException>(() => {
-                resetEvent.Wait(new CancellationTokenSource(100).Token);
-            });
+                Assert.Throws<TaskCanceledException>(() => { resetEvent.Wait(new CancellationTokenSource(100).Token); });
 
-            await Task.Delay(110);
-            Assert.Equal(2, hits);
+                await Task.Delay(110);
+                Assert.Equal(2, hits);
+            }
         }
 
         [Fact]
         public async Task CanRunConcurrent() {
-            int hits = 0;
             Log.SetLogLevel<ScheduledTimer>(LogLevel.Trace);
 
-            var timer = new ScheduledTimer(() => {
+            int hits = 0;
+            Func<Task<DateTime?>> callback = () => {
                 int i = Interlocked.Increment(ref hits);
                 _logger.Info($"Running {i}...");
                 return Task.FromResult<DateTime?>(null);
-            }, minimumIntervalTime: TimeSpan.FromMilliseconds(250), loggerFactory: Log);
+            };
 
-            for (int i = 0; i < 5; i++) {
-                timer.ScheduleNext();
-                await Task.Delay(5);
+            using (var timer = new ScheduledTimer(callback, minimumIntervalTime: TimeSpan.FromMilliseconds(100), loggerFactory: Log)) {
+                for (int i = 0; i < 5; i++) {
+                    timer.ScheduleNext();
+                    await Task.Delay(5);
+                }
+
+                await Task.Delay(250);
+                Assert.Equal(2, hits);
+
+                await Task.Delay(100);
+                Assert.Equal(2, hits);
             }
-
-            await Task.Delay(1000);
-
-            Assert.Equal(2, hits);
-
-            await Task.Delay(100);
-            Assert.Equal(2, hits);
         }
     }
 }
