24c1835993cda932d427b35a48486afc640ec424
StephenCleary <scleary@learningmachine.com>
2016-09-15 15:54:26 -0500

    Removed CanSleep test from SystemClock due to race condition where SystemClock.SleepAsync may be called after the TestSystemClock has been advanced.

--- test/Foundatio.Tests/Utility/SystemClockTests.cs
+++ test/Foundatio.Tests/Utility/SystemClockTests.cs
@@ -15,30 +15,6 @@ namespace Foundatio.Tests.Utility
         }
 
         [Fact]
-        public async Task CanSleep()
-        {
-            var sw = Stopwatch.StartNew();
-            SystemClock.Sleep(250);
-            sw.Stop();
-
-            Assert.InRange(sw.ElapsedMilliseconds, 225, 400);
-
-            using (TestSystemClock.Install()) {
-                var now = SystemClock.UtcNow;
-                sw.Restart();
-                var task = Task.Run(() => SystemClock.Sleep(1000));
-                TestSystemClock.AdvanceBy(TimeSpan.FromMilliseconds(1000));
-                await task;
-                sw.Stop();
-                var afterSleepNow = SystemClock.UtcNow;
-
-                Assert.InRange(sw.ElapsedMilliseconds, 0, 25);
-                Assert.True(afterSleepNow > now);
-                Assert.InRange(afterSleepNow.Subtract(now).TotalMilliseconds, 950, 1100);
-            }
-        }
-
-        [Fact]
         public async Task CanSleepAsync()
         {
             var sw = Stopwatch.StartNew();
