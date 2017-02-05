77a612d70853a16295cd777cb1c86c674f3b826d
Blake Niemyjski <bniemyjski@gmail.com>
2017-02-05 09:37:21 -0600

    Fixed a compiler error

--- samples/Foundatio.RabbitMQPublishConsole/Program.cs
+++ samples/Foundatio.RabbitMQPublishConsole/Program.cs
@@ -4,16 +4,15 @@ using Foundatio.Messaging;
 namespace Foundatio.RabbitMQPublishConsole {
     public class Program {
         public static void Main(string[] args) {
-            IMessageBus messageBus;
-            string message;
             Console.WriteLine("Publisher...");
-            messageBus = new RabbitMQMessageBus("guest", "guest", "FoundatioExchangeFanout", "FoundatioExchangeFanoutQueue");
+            IMessageBus messageBus = new RabbitMQMessageBus("amqp://localhost", "FoundatioQueue", "FoundatioQueueRoutingKey", "FoundatioExchange");
 
             Console.WriteLine("Enter the messages to send (press CTRL+Z) to exit :");
 
+            string message;
             do {
                 message = Console.ReadLine();
-                    messageBus.PublishAsync(message);
+                messageBus.PublishAsync(message);
             } while (message != null);
 
             messageBus.Dispose();
