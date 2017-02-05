4ee9d9f3843736c009bd901d18ce0eec3d6c6ec3
Blake Niemyjski <bniemyjski@gmail.com>
2017-02-05 09:35:30 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master' into feature/rabbitmq
    # Conflicts:
    #	samples/Foundatio.RabbitMQPublishConsole/Program.cs
    #	samples/Foundatio.RabbitMQSubscribeConsole/Program.cs
    #	src/Foundatio.RabbitMQ/Messaging/RabbitMQMessageBus.cs
    #	test/Foundatio.RabbitMQ.Tests/Messaging/RabbitMqMessageBusTests.cs

---
+++ src/Foundatio/Extensions/CacheClientExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foundatio.Utility;
+using Foundatio.Extensions;
+
+namespace Foundatio.Caching {
+    public static class CacheClientExtensions {
+        public static async Task<T> GetAsync<T>(this ICacheClient client, string key, T defaultValue) {
+            var cacheValue = await client.GetAsync<T>(key).AnyContext();
+            return cacheValue.HasValue ? cacheValue.Value : defaultValue;
+        }
+
+        public static Task<IDictionary<string, CacheValue<T>>> GetAllAsync<T>(this ICacheClient client, params string[] keys) {
+            return client.GetAllAsync<T>(keys.ToArray());
+        }
+
+        public static async Task<bool> RemoveAsync(this ICacheClient client, string key) {
+            return await client.RemoveAllAsync(new[] { key }).AnyContext() == 1;
+        }
+
+        public static async Task<long> IncrementAsync(this ICacheClient client, string key, int amount = 1, TimeSpan? expiresIn = null) {
+            var result = await client.IncrementAsync(key, amount, expiresIn).AnyContext();
+            return (long)result;
+        }
+
+        public static Task<long> IncrementAsync(this ICacheClient client, string key, int amount = 1, DateTime? expiresAtUtc = null) {
+            return IncrementAsync(client, key, amount, expiresAtUtc?.Subtract(SystemClock.UtcNow));
+        }
+
+        public static Task<long> DecrementAsync(this ICacheClient client, string key, int amount = 1, TimeSpan? expiresIn = null) {
+            return IncrementAsync(client, key, amount, expiresIn);
+        }
+
+        public static Task<long> DecrementAsync(this ICacheClient client, string key, int amount = 1, DateTime? expiresAtUtc = null) {
+            return client.IncrementAsync(key, -amount, expiresAtUtc);
+        }
+        
+        public static Task<bool> AddAsync<T>(this ICacheClient client, string key, T value, DateTime? expiresAtUtc = null) {
+            return client.AddAsync(key, value, expiresAtUtc?.Subtract(SystemClock.UtcNow));
+        }
+
+        public static Task<bool> SetAsync<T>(this ICacheClient client, string key, T value, DateTime? expiresAtUtc = null) {
+            return client.SetAsync(key, value, expiresAtUtc?.Subtract(SystemClock.UtcNow));
+        }
+    
+        public static Task<bool> ReplaceAsync<T>(this ICacheClient client, string key, T value, DateTime? expiresAtUtc = null) {
+            return client.ReplaceAsync(key, value, expiresAtUtc?.Subtract(SystemClock.UtcNow));
+        }
+        
+        public static Task<int> SetAllAsync(this ICacheClient client, IDictionary<string, object> values, DateTime? expiresAtUtc = null) {
+            return client.SetAllAsync(values, expiresAtUtc?.Subtract(SystemClock.UtcNow));
+        }
+        
+        public static Task SetExpirationAsync(this ICacheClient client, string key, DateTime expiresAtUtc) {
+            return client.SetExpirationAsync(key, expiresAtUtc.Subtract(SystemClock.UtcNow));
+        }
+
+        public static async Task<bool> SetAddAsync<T>(this ICacheClient client, string key, T value, TimeSpan? expiresIn = null) {
+            return await client.SetAddAsync(key, new [] { value }, expiresIn).AnyContext() > 0;
+        }
+
+        public static async Task<bool> SetRemoveAsync<T>(this ICacheClient client, string key, T value, TimeSpan? expiresIn = null) {
+            return await client.SetRemoveAsync(key, new[] { value }, expiresIn).AnyContext() > 0;
+        }
+    }
+}
---
+++ test/Foundatio.Tests/Utility/SystemClockTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Foundatio.Logging.Xunit;
+using Foundatio.Utility;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Foundatio.Tests.Utility {
+    public class SystemClockTests : TestWithLoggingBase {
+        public SystemClockTests(ITestOutputHelper output) : base(output) {}
+
+        [Fact]
+        public void CanGetTime() {
+            using (TestSystemClock.Install()) {
+                var now = DateTime.UtcNow;
+                SystemClock.Test.SetFixedTime(now);
+                Assert.Equal(now, SystemClock.UtcNow);
+                Assert.Equal(now.ToLocalTime(), SystemClock.Now);
+                Assert.Equal(now, SystemClock.OffsetUtcNow);
+                Assert.Equal(now.ToLocalTime(), SystemClock.OffsetNow);
+                Assert.Equal(DateTimeOffset.Now.Offset, SystemClock.TimeZoneOffset);
+            }
+        }
+
+        [Fact]
+        public void CanSleep() {
+            using (TestSystemClock.Install()) {
+                var sw = Stopwatch.StartNew();
+                SystemClock.Sleep(250);
+                sw.Stop();
+                Assert.InRange(sw.ElapsedMilliseconds, 225, 400);
+
+                SystemClock.Test.UseFakeSleep();
+
+                var now = SystemClock.UtcNow;
+                sw.Restart();
+                SystemClock.Sleep(1000);
+                sw.Stop();
+                var afterSleepNow = SystemClock.UtcNow;
+
+                Assert.InRange(sw.ElapsedMilliseconds, 0, 30);
+                Assert.True(afterSleepNow > now);
+                Assert.InRange(afterSleepNow.Subtract(now).TotalMilliseconds, 950, 1100);
+            }
+        }
+
+        [Fact]
+        public async Task CanSleepAsync() {
+            using (TestSystemClock.Install()) {
+                var sw = Stopwatch.StartNew();
+                await SystemClock.SleepAsync(250);
+                sw.Stop();
+
+                Assert.InRange(sw.ElapsedMilliseconds, 225, 400);
+
+                SystemClock.Test.UseFakeSleep();
+
+                var now = SystemClock.UtcNow;
+                sw.Restart();
+                await SystemClock.SleepAsync(1000);
+                sw.Stop();
+                var afterSleepNow = SystemClock.UtcNow;
+
+                Assert.InRange(sw.ElapsedMilliseconds, 0, 30);
+                Assert.True(afterSleepNow > now);
+                Assert.InRange(afterSleepNow.Subtract(now).TotalMilliseconds, 950, 1100);
+            }
+        }
+
+        [Fact]
+        public void CanSetTimeZone() {
+            using (TestSystemClock.Install()) {
+                var utcNow = DateTime.UtcNow;
+                var now = new DateTime(utcNow.AddHours(1).Ticks, DateTimeKind.Local);
+                SystemClock.Test.SetFixedTime(utcNow);
+                SystemClock.Test.SetTimeZoneOffset(TimeSpan.FromHours(1));
+
+                Assert.Equal(utcNow, SystemClock.UtcNow);
+                Assert.Equal(utcNow, SystemClock.OffsetUtcNow);
+                Assert.Equal(now, SystemClock.Now);
+                Assert.Equal(new DateTimeOffset(now.Ticks, TimeSpan.FromHours(1)), SystemClock.OffsetNow);
+                Assert.Equal(TimeSpan.FromHours(1), SystemClock.TimeZoneOffset);
+            }
+        }
+
+        [Fact]
+        public void CanSetLocalFixedTime() {
+            using (TestSystemClock.Install()) {
+                var now = DateTime.Now;
+                var utcNow = now.ToUniversalTime();
+                SystemClock.Test.SetFixedTime(now);
+
+                Assert.Equal(now, SystemClock.Now);
+                Assert.Equal(now, SystemClock.OffsetNow);
+                Assert.Equal(utcNow, SystemClock.UtcNow);
+                Assert.Equal(utcNow, SystemClock.OffsetUtcNow);
+                Assert.Equal(DateTimeOffset.Now.Offset, SystemClock.TimeZoneOffset);
+            }
+        }
+
+        [Fact]
+        public void CanSetUtcFixedTime() {
+            using (TestSystemClock.Install()) {
+                var utcNow = DateTime.UtcNow;
+                var now = utcNow.ToLocalTime();
+                SystemClock.Test.SetFixedTime(utcNow);
+
+                Assert.Equal(now, SystemClock.Now);
+                Assert.Equal(now, SystemClock.OffsetNow);
+                Assert.Equal(utcNow, SystemClock.UtcNow);
+                Assert.Equal(utcNow, SystemClock.OffsetUtcNow);
+                Assert.Equal(DateTimeOffset.Now.Offset, SystemClock.TimeZoneOffset);
+            }
+        }
+    }
+}
