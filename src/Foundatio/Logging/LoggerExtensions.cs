c4529577880047b72a870ddb0ab221b1ee7e7a90
Paul Welter <pwelter@loresoft.com>
2017-09-20 08:22:16 -0500

    add BeginScope as dictionary extension method

--- src/Foundatio/Logging/LoggerExtensions.cs
+++ src/Foundatio/Logging/LoggerExtensions.cs
@@ -1,13 +1,16 @@
 using System;
-using Foundatio.Logging;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Logging.Internal;
 
 namespace Foundatio.Logging {
     public static class LoggerExtensions {
         public static ILogger GetLogger(this object target) {
             return target is IHaveLogger accessor ? accessor.Logger : NullLogger.Instance;
+        }
+
+        public static IDisposable BeginScope(this ILogger logger, string key, object value) {
+            return logger.BeginScope(new Dictionary<string, object> { { key, value } });
         }
     }
 }
