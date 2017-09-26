0f26c5d44d37ed3bbae7c20d969b3c52208771df
Eric J. Smith <eric@codesmithtools.com>
2017-09-26 17:02:08 -0500

    Remove windows specific code.

--- src/Foundatio/Jobs/JobRunner.cs
+++ src/Foundatio/Jobs/JobRunner.cs
@@ -145,9 +145,9 @@ namespace Foundatio.Jobs {
                     return _jobShutdownCancellationTokenSource.Token;
 
                 _jobShutdownCancellationTokenSource = new CancellationTokenSource();
-                ShutdownEventCatcher.Shutdown += args => {
+                Console.CancelKeyPress += (sender, args) => {
                     _jobShutdownCancellationTokenSource.Cancel();
-                    logger?.LogInformation("Job shutdown event signaled: {0}", args.Reason);
+                    logger?.LogInformation("Job shutdown event signaled: {0}", args.SpecialKey);
                 };
 
                 string webJobsShutdownFile = Environment.GetEnvironmentVariable("WEBJOBS_SHUTDOWN_FILE");
