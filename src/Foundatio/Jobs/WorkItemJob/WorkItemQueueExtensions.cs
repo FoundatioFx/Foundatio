b52089c21dc63cdb6ddf839d323ba32ea89c479b
Blake Niemyjski <bniemyjski@gmail.com>
2017-09-30 09:55:31 -0500

    Reverted queue extension as it wasn't returning job id.

--- src/Foundatio/Jobs/WorkItemJob/WorkItemQueueExtensions.cs
+++ src/Foundatio/Jobs/WorkItemJob/WorkItemQueueExtensions.cs
@@ -6,15 +6,17 @@ using Foundatio.Serializer;
 
 namespace Foundatio.Jobs {
     public static class WorkItemQueueExtensions {
-        public static Task<string> EnqueueAsync<T>(this IQueue<WorkItemData> queue, T workItemData, bool includeProgressReporting = false) {
-            string id = Guid.NewGuid().ToString("N");
+        public static async Task<string> EnqueueAsync<T>(this IQueue<WorkItemData> queue, T workItemData, bool includeProgressReporting = false) {
+            string jobId = Guid.NewGuid().ToString("N");
             string json = queue.Serializer.SerializeToString(workItemData);
-            return queue.EnqueueAsync(new WorkItemData {
+            await queue.EnqueueAsync(new WorkItemData {
                 Data = json,
-                WorkItemId = id,
+                WorkItemId = jobId,
                 Type = typeof(T).AssemblyQualifiedName,
                 SendProgressReports = includeProgressReporting
-            });
+            }).AnyContext();
+
+            return jobId;
         }
     }
 }
