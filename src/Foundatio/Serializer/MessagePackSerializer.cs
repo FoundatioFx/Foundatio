4bf69430c04de230686a0c6f4e91bb11d801e87d
Edward Meng <edward.meng@gaiaworks.cn>
2018-01-25 09:11:28 +0800

    Add a optional parameter for MessagePackSerializer to determine whether compress the serialized data.

--- src/Foundatio/Serializer/MessagePackSerializer.cs
+++ src/Foundatio/Serializer/MessagePackSerializer.cs
@@ -6,17 +6,29 @@ using MessagePack.Resolvers;
 namespace Foundatio.Serializer {
     public class MessagePackSerializer : ISerializer {
         private readonly IFormatterResolver _formatterResolver;
+        private readonly bool _compressEnabled;
 
-        public MessagePackSerializer(IFormatterResolver resolver = null) {
+        public MessagePackSerializer(IFormatterResolver resolver = null, bool compressEnabled = false) {
+            _compressEnabled = compressEnabled;
             _formatterResolver = resolver ?? ContractlessStandardResolver.Instance;
         }
 
         public void Serialize(object data, Stream output) {
-            MessagePack.MessagePackSerializer.NonGeneric.Serialize(data.GetType(), output, data, _formatterResolver);
+            if (_compressEnabled) {
+                MessagePack.LZ4MessagePackSerializer.NonGeneric.Serialize(data.GetType(), output, data, _formatterResolver);
+            }
+            else {
+                MessagePack.MessagePackSerializer.NonGeneric.Serialize(data.GetType(), output, data, _formatterResolver);
+            }
         }
 
         public object Deserialize(Stream input, Type objectType) {
-            return MessagePack.MessagePackSerializer.NonGeneric.Deserialize(objectType, input, _formatterResolver);
+            if (_compressEnabled) {
+                return MessagePack.LZ4MessagePackSerializer.NonGeneric.Deserialize(objectType, input, _formatterResolver);
+            }
+            else {
+                return MessagePack.MessagePackSerializer.NonGeneric.Deserialize(objectType, input, _formatterResolver);
+            }
         }
     }
 }
