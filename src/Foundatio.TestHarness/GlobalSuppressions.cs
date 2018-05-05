055324ea6ff438523740491ec040e94d92a07de2
Eric J. Smith <eric@codesmithtools.com>
2018-05-05 18:34:54 -0500

    Revert and add warning suppression

---
+++ src/Foundatio.TestHarness/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("AsyncUsage", "AsyncFixer02:Long running or blocking operations under an async method", Justification = "<Pending>", Scope = "member", Target = "~M:Foundatio.Tests.Storage.FileStorageTestsBase.CanSaveFilesAsync~System.Threading.Tasks.Task")]
+
