4123c1da1bc1251656ee89137d70552e3566d56e
edward.meng <edward.meng@gaiaworks.cn>
2018-08-15 13:33:14 +0800

    Add implementation of metrics client with AppMetrics.

---
+++ src/Foundatio.AppMetrics/AppMetricsClient.cs
@@ -0,0 +1,28 @@
+﻿using App.Metrics;
+using App.Metrics.Counter;
+using App.Metrics.Gauge;
+using App.Metrics.Timer;
+
+namespace Foundatio.Metrics {
+    public class AppMetricsClient : IMetricsClient {
+        private readonly IMetrics _metrics;
+
+        public AppMetricsClient(IMetrics metrics) {
+            _metrics = metrics;
+        }
+
+        public void Dispose() { }
+
+        public void Counter(string name, int value = 1) {
+            _metrics.Provider.Counter.Instance(new CounterOptions {Name = name}).Increment(value);
+        }
+
+        public void Gauge(string name, double value) {
+            _metrics.Provider.Gauge.Instance(new GaugeOptions {Name = name}).SetValue(value);
+        }
+
+        public void Timer(string name, int milliseconds) {
+            _metrics.Provider.Timer.Instance(new TimerOptions {Name = name}).Record(milliseconds, TimeUnit.Milliseconds);
+        }
+    }
+}
