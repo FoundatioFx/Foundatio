153d4b6cf16242170a53101d6b9540768617d080
Eric J. Smith <eric@codesmithtools.com>
2018-09-06 00:14:07 -0500

    Minor changes

--- src/Foundatio.Jobs.Commands/JobCommands.cs
+++ src/Foundatio.Jobs.Commands/JobCommands.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using Foundatio.Jobs.Commands.Extensions;
 using Foundatio.Utility;
