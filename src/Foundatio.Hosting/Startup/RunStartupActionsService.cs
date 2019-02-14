76efa0e7be2fec2def3ea705a0faa699a5772e97
Eric J. Smith <eric@codesmithtools.com>
2019-02-13 21:12:25 -0600

    Log that we are running startup actions

--- src/Foundatio.Hosting/Startup/RunStartupActionsService.cs
+++ src/Foundatio.Hosting/Startup/RunStartupActionsService.cs
@@ -1,21 +1,26 @@
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Foundatio.Startup;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Foundatio.Hosting.Startup {
     public class RunStartupActionsService : BackgroundService {
         private readonly StartupContext _startupContext;
         private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger _logger;
 
-        public RunStartupActionsService(StartupContext startupContext, IServiceProvider serviceProvider) {
+        public RunStartupActionsService(StartupContext startupContext, IServiceProvider serviceProvider, ILogger<RunStartupActionsService> logger) {
             _startupContext = startupContext;
             _serviceProvider = serviceProvider;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+            _logger.LogInformation("Running startup actions...");
             await _serviceProvider.RunStartupActionsAsync(stoppingToken).ConfigureAwait(false);
+            _logger.LogInformation("Startup actions complete.");
             _startupContext.MarkStartupComplete();
         }
     }
