683d79521abac3992acf6d22e0fa47e23bad69db
Eric J. Smith <eric@codesmithtools.com>
2019-02-16 15:20:54 -0600

    Couple minor changes

--- samples/Foundatio.HostingSample/MyCriticalHealthCheck.cs
+++ samples/Foundatio.HostingSample/MyCriticalHealthCheck.cs
@@ -7,8 +7,10 @@ namespace Foundatio.HostingSample {
     public class MyCriticalHealthCheck : IHealthCheck {
         private static DateTime _startTime = DateTime.Now;
 
-        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken()) {
-            return DateTime.Now.Subtract(_startTime) > TimeSpan.FromSeconds(10) ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy("Critical resource not available.");
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken()) {
+            return DateTime.Now.Subtract(_startTime) > TimeSpan.FromSeconds(10) ?
+                Task.FromResult(HealthCheckResult.Healthy())
+                : Task.FromResult(HealthCheckResult.Unhealthy("Critical resource not available."));
         }
     }
 }
