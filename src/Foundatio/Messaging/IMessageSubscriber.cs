7cec07524365d37f0ac90f80834f1a152d0f0825
Eric J. Smith <eric@codesmithtools.com>
2019-03-07 00:50:27 -0600

    Remove priority for now, can be added later

--- src/Foundatio/Messaging/IMessageSubscriber.cs
+++ src/Foundatio/Messaging/IMessageSubscriber.cs
@@ -4,11 +4,6 @@ using System.Threading.Tasks;
 using Foundatio.Utility;
 
 namespace Foundatio.Messaging {
-    // conventions to determine message queue based on message type
-    // conventions for determining default time to live, retries, and deadletter behavior based on message type
-    // pub/sub you would publish messages and each subscriber would automatically get a unique subscriber id and the messages would go to all of them
-    // worker you would publish messages and each subscriber would use the same subscriber id and the messages would get round robin'd
-    // need to figure out how we would handle rpc request / response. need to be able to subscribe for a single message
     public interface IMessageSubscriber {
         // there will be extensions that allow subscribing via generic message type parameters with and without the message context wrapper
         Task<IMessageSubscription> SubscribeAsync(Func<IMessageContext, Task> handler, IMessageSubscriptionOptions options);
@@ -17,16 +12,6 @@ namespace Foundatio.Messaging {
     public interface IMessageSubscriptionOptions {
         // message type for the subscription
         Type MessageType { get; }
-        // subscription id, for worker queues use the same subscription id for all subscriptions and the messages will be round robin'd
-        string SubscriptionId { get; set; }
-        // how many messages should be fetched at a time
-        int? PrefetchSize { get; set; }
-        // what priority level messages should this subscription receive
-        int? Priority { get; set; }
-        // how long should the message remain in flight before timing out
-        TimeSpan? TimeToLive { get; set; }
-        // how messages should be acknowledged
-        AcknowledgementStrategy? AcknowledgementStrategy { get; set; }
     }
 
     public interface IMessageSubscription : IDisposable {
