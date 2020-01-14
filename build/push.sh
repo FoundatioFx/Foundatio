f612cd785049a7133c943e93184234d7fabc5657
Eric J. Smith <eric@codesmithtools.com>
2020-01-14 11:47:17 -0600

    Push CI to GPR and remove push script

--- build/push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-for package in $(find -name "*.nupkg" | grep "minver" -v); do
-  echo "${0##*/}": Pushing $package...
-  dotnet nuget push $package --source $1 --api-key $2
-done
