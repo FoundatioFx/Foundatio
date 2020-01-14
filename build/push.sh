c07f909a2d5c91b92995b56e339a86572596015b
Eric J. Smith <eric@ericjsmith.com>
2020-01-13 22:21:59 -0600

    Don't publish minver

--- build/push.sh
+++ build/push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for package in $(find -name "*.nupkg"); do
+for package in $(find -name "*.nupkg" | grep "minver" -v); do
   echo "${0##*/}": Pushing $package...
   dotnet nuget push $package --source $1 --api-key $2
 done
