b24318c167abea22d79c494d3caa1a3683970ede
Eric J. Smith <eric@ericjsmith.com>
2020-01-13 21:53:22 -0600

    Push to nuget on tag and output build version

--- build/push.sh
+++ build/push.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 for package in $(find -name "*.nupkg"); do
   echo "${0##*/}": Pushing $package...
-  dotnet nuget push $package --source https://f.feedz.io/foundatio/foundatio/nuget --api-key $1
+  dotnet nuget push $package --source $1 --api-key $2
 done
