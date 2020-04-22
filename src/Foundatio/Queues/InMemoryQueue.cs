ecb092676d7e04b96bdba8dc22771a84090c0c94
Eric J. Smith <eric@codesmithtools.com>
2020-04-22 18:18:21 -0500

    Some more queue options changes and try to fix statsd tests not working on osx

--- src/Foundatio/Queues/InMemoryQueue.cs
+++ src/Foundatio/Queues/InMemoryQueue.cs
@@ -55,15 +55,16 @@ namespace Foundatio.Queues {
             return new ReadOnlyCollection<QueueEntry<T>>(_queue.ToList());
         }
 
-        protected override async Task<string> EnqueueImplAsync(T data, QueueOptions options = null) {
-            string id = Guid.NewGuid().ToString("N");
+        protected override async Task<string> EnqueueImplAsync(T data, QueueEntryOptions options = null) {
+            options ??= new QueueEntryOptions { Id = Guid.NewGuid().ToString() };
+            string id = options?.Id ?? Guid.NewGuid().ToString("N");
             bool isTraceLogLevelEnabled = _logger.IsEnabled(LogLevel.Trace);
             if (isTraceLogLevelEnabled) _logger.LogTrace("Queue {Name} enqueue item: {Id}", _options.Name, id);
 
             if (!await OnEnqueuingAsync(data, options).AnyContext())
                 return null;
 
-            var entry = new QueueEntry<T>(id, options?.CorrelationId, options?.ParentId, data.DeepClone(), this, SystemClock.UtcNow, 0);
+            var entry = new QueueEntry<T>(id, options?.CorrelationId, data.DeepClone(), this, SystemClock.UtcNow, 0);
             entry.Properties.AddRange(options?.Properties);
             
             _queue.Enqueue(entry);
@@ -159,7 +160,7 @@ namespace Foundatio.Queues {
             Interlocked.Increment(ref _dequeuedCount);
             if (isTraceLogLevelEnabled) _logger.LogTrace("Dequeue: Got Item");
 
-            var entry = new QueueEntry<T>(info.Id, info.CorrelationId, info.ParentId, info.Value.DeepClone(), this, info.EnqueuedTimeUtc, info.Attempts);
+            var entry = new QueueEntry<T>(info.Id, info.CorrelationId, info.Value.DeepClone(), this, info.EnqueuedTimeUtc, info.Attempts);
             entry.Properties.AddRange(info.Properties);
             
             await entry.RenewLockAsync();
