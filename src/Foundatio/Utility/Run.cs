e00f636faa0aa77ae09ee95a633a3250303d3ef6
Eric J. Smith <eric@codesmithtools.com>
2020-07-29 00:04:42 -0500

    Fix issue with retry util not bubbling up the correct exception

--- src/Foundatio/Utility/Run.cs
+++ src/Foundatio/Utility/Run.cs
@@ -19,8 +19,11 @@ namespace Foundatio.Utility {
             return Task.WhenAll(Enumerable.Range(1, iterations).Select(i => Task.Run(() => work(i))));
         }
 
-        public static Task WithRetriesAsync(Func<Task> action, int maxAttempts = 5, TimeSpan? retryInterval = null, CancellationToken cancellationToken = default, ILogger logger = null) {
-            return WithRetriesAsync(() => action().ContinueWith(t => Task.CompletedTask, TaskContinuationOptions.OnlyOnRanToCompletion), maxAttempts, retryInterval, cancellationToken, logger);
+        public static async Task WithRetriesAsync(Func<Task> action, int maxAttempts = 5, TimeSpan? retryInterval = null, CancellationToken cancellationToken = default, ILogger logger = null) {
+            await WithRetriesAsync<object>(async () => {
+                await action().AnyContext();
+                return null;
+            }, maxAttempts, retryInterval, cancellationToken, logger);
         }
 
         public static async Task<T> WithRetriesAsync<T>(Func<Task<T>> action, int maxAttempts = 5, TimeSpan? retryInterval = null, CancellationToken cancellationToken = default, ILogger logger = null) {
