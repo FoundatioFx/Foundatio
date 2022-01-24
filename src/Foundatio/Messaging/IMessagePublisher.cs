846542ca32a99a2c6cde5e7f1383408779da7e34
JC <justin@global-roam.com>
2022-01-25 03:31:26 +1000

    Support for UniqueId on QueueEntryOptions and add MessageOptions (#268)
    * support for ability for the message bus to send messages to subscriptions synchronously
    
    * modifying the default option for synchronously sending messages on a message bus
    
    * small tidy up
    
    * taking a different approach to the problem of subscribers throwing exceptions
    
    * tidying up the exception handling and syntax
    
    * bringing more in line with the original code
    
    * fixing whitespace
    
    * fixing whitespace (take 2)
    
    * fixing up the task that calls the subscriber handler
    
    * modifying to use Task.Run rather than Task.Factory.StartNew
    
    * improving tests to tolerate subscriber failure
    
    * tidying up
    
    * improving test that tolerates subscriber failure
    
    * making SendMessageToSubscribers async
    
    * added a log message in the in memory message bus if sending message to subscribers fails
    
    * Added Id to QueueEntryOptions
    
    * updated InMemoryMessageBus with new interface to support options
    
    * renamed identifier on QueueEntryOptions to be UniqueId
    
    * switched MessageBus to use a new MessageOptions instead of QueueEntryOptions
    
    * added properties and correlation id to message options

--- src/Foundatio/Messaging/IMessagePublisher.cs
+++ src/Foundatio/Messaging/IMessagePublisher.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Foundatio.Queues;
 
 namespace Foundatio.Messaging {
     public interface IMessagePublisher {
-        Task PublishAsync(Type messageType, object message, TimeSpan? delay = null, CancellationToken cancellationToken = default);
+        Task PublishAsync(Type messageType, object message, TimeSpan? delay = null, MessageOptions options = null, CancellationToken cancellationToken = default);
     }
 
     public static class MessagePublisherExtensions {
-        public static Task PublishAsync<T>(this IMessagePublisher publisher, T message, TimeSpan? delay = null) where T : class {
-            return publisher.PublishAsync(typeof(T), message, delay);
+        public static Task PublishAsync<T>(this IMessagePublisher publisher, T message, TimeSpan? delay = null, MessageOptions options = null) where T : class {
+            return publisher.PublishAsync(typeof(T), message, delay, options);
         }
     }
 }
