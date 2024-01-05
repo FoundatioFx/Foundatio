eb9a76cc97b73e90e56cc6a57f5d1d989f61d5e7
Blake Niemyjski <bniemyjski@gmail.com>
2024-01-05 14:11:43 -0600

    Fixed unsaved file

--- src/Foundatio/Messaging/NullMessageBus.cs
+++ src/Foundatio/Messaging/NullMessageBus.cs
@@ -2,21 +2,21 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Foundatio.Messaging
+namespace Foundatio.Messaging;
+
+public class NullMessageBus : IMessageBus
 {
-    public class NullMessageBus : IMessageBus
+    public static readonly NullMessageBus Instance = new();
+
+    public Task PublishAsync(Type messageType, object message, MessageOptions options = null, CancellationToken cancellationToken = default)
     {
-        public static readonly NullMessageBus Instance = new();
-
-        public Task PublishAsync(Type messageType, object message, MessageOptions options = null, CancellationToken cancellationToken = default)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task SubscribeAsync<T>(Func<T, CancellationToken, Task> handler, CancellationToken cancellationToken = default) where T : class
-        {
-            return Task.CompletedTask;
-        }
-
-        public void Dispose() { }
+        return Task.CompletedTask;
     }
+
+    public Task SubscribeAsync<T>(Func<T, CancellationToken, Task> handler, CancellationToken cancellationToken = default) where T : class
+    {
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() { }
+}
