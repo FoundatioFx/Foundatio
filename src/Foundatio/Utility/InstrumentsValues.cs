e60b8f467fced2087749fd02e92706baffa92e14
Eric J. Smith <eric@codesmithtools.com>
2024-08-31 01:17:39 -0500

    Fix issue with queue metrics instrument null reference

--- src/Foundatio/Utility/InstrumentsValues.cs
+++ src/Foundatio/Utility/InstrumentsValues.cs
@@ -24,33 +24,38 @@ public class InstrumentsValues<T1, T2, T3>
     public T1 GetValue1()
     {
         if (!_value1.HasValue)
-        {
             (_value1, _value2, _value3) = UpdateValues();
-        }
+
+        if (_value1 == null)
+            return default;
 
         T1 value = _value1.Value;
         _value1 = null;
         return value;
+
     }
 
     public T2 GetValue2()
     {
         if (!_value2.HasValue)
-        {
             (_value1, _value2, _value3) = UpdateValues();
-        }
+
+        if (_value2 == null)
+            return default;
 
         T2 value = _value2.Value;
         _value2 = null;
         return value;
+
     }
 
     public T3 GetValue3()
     {
         if (!_value3.HasValue)
-        {
             (_value1, _value2, _value3) = UpdateValues();
-        }
+
+        if (_value3 == null)
+            return default;
 
         T3 value = _value3.Value;
         _value3 = null;
