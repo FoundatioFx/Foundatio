0e200d6c95736bd6f23e03cf18d498bb74dacaa7
Blake Niemyjski <bniemyjski@gmail.com>
2024-10-02 11:25:16 -0500

    Added Async Disposable Action

---
+++ src/Foundatio/Utility/AsyncDisposableAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foundatio.Utility;
+
+/// <summary>
+/// A class that will call an <see cref="Func{TResult}"/> when Disposed.
+/// </summary>
+public sealed class AsyncDisposableAction : IAsyncDisposable
+{
+    private Func<Task> _exitTask;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DisposableAction"/> class.
+    /// </summary>
+    /// <param name="exitTask">The exit action.</param>
+    public AsyncDisposableAction(Func<Task> exitTask)
+    {
+        _exitTask = exitTask;
+    }
+
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        var exitAction = Interlocked.Exchange(ref _exitTask, null);
+        if (exitAction is not null)
+            await _exitTask().AnyContext();
+    }
+}
