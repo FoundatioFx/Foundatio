52bf42750cf37e92dd624a90b3836da92088a72e
Blake Niemyjski <bniemyjski@gmail.com>
2025-05-01 09:06:25 -0500

    Added ability to detect start-services.ps1 and use it

--- start-all-services.ps1
+++ start-all-services.ps1
@@ -2,8 +2,15 @@ $parentDirectory = Split-Path -Path $PSScriptRoot -Parent
 $directories = Get-ChildItem -Path $parentDirectory -Directory -Filter "Foundatio.*"
 
 foreach ($directory in $directories) {
+    $startScriptPath = Join-Path -Path $directory.FullName -ChildPath "start-services.ps1"
     $composeFilePath = Join-Path -Path $directory.FullName -ChildPath "docker-compose.yml"
-    if (Test-Path -Path $composeFilePath) {
+
+    if (Test-Path -Path $startScriptPath) {
+        Set-Location -Path $directory.FullName
+        Write-Host "Starting services in directory using start-services.ps1: $($directory.FullName)"
+        & $startScriptPath
+    }
+    elseif (Test-Path -Path $composeFilePath) {
         Set-Location -Path $directory.FullName
         Write-Host "Starting services in directory: $($directory.FullName)"
         docker compose -f $composeFilePath up --detach
