a0326f3b18213a45a3f5a232e1aabee59be172b3
Blake Niemyjski <bniemyjski@gmail.com>
2026-01-30 14:18:13 -0600

    Add DeepClone benchmarks, upgrade DeepCloner to v0.10.4, and add sync resilience methods (#445)
    * DeepClone benchmarks
    
    * Upgrade DeepCloner to v0.10.4
    
    The main change between v0.10.3 and v0.10.4 is:
    Excludes default comparers from cloning to fix issues with .NET 6 and HashSet (the EqualityComparer handling I mentioned earlier)
    So it's a minor but important bug fix release, particularly relevant for .NET 6+ compatibility.

--- src/Foundatio/DeepCloner/Helpers/DeepClonerGenerator.cs
+++ src/Foundatio/DeepCloner/Helpers/DeepClonerGenerator.cs
@@ -1,231 +1,233 @@
-﻿#define NETCORE
+#define NETCORE
 using System;
 using System.Linq;
 
-namespace Foundatio.Force.DeepCloner.Helpers;
-
-internal static class DeepClonerGenerator
+namespace Foundatio.Force.DeepCloner.Helpers
 {
-    public static T CloneObject<T>(T obj)
-    {
-        if (obj is ValueType)
-        {
-            var type = obj.GetType();
-            if (typeof(T) == type)
-            {
-                if (DeepClonerSafeTypes.CanReturnSameObject(type))
-                    return obj;
+	internal static class DeepClonerGenerator
+	{
+		public static T CloneObject<T>(T obj)
+		{
+			if (obj is ValueType)
+			{
+				var type = obj.GetType();
+				if (typeof(T) == type)
+				{
+					if (DeepClonerSafeTypes.CanReturnSameObject(type))
+						return obj;
 
-                return CloneStructInternal(obj, new DeepCloneState());
-            }
-        }
+					return CloneStructInternal(obj, new DeepCloneState());
+				}
+			}
 
-        return (T)CloneClassRoot(obj);
-    }
+			return (T)CloneClassRoot(obj);
+		}
 
-    private static object CloneClassRoot(object obj)
-    {
-        if (obj == null)
-            return null;
+		private static object CloneClassRoot(object obj)
+		{
+			if (obj == null)
+				return null;
+		
+			var cloner = (Func<object, DeepCloneState, object>)DeepClonerCache.GetOrAddClass(obj.GetType(), t => GenerateCloner(t, true));
 
-        var cloner = (Func<object, DeepCloneState, object>)DeepClonerCache.GetOrAddClass(obj.GetType(), t => GenerateCloner(t, true));
+			// null -> should return same type
+			if (cloner == null) 
+				return obj;
 
-        // null -> should return same type
-        if (cloner == null)
-            return obj;
+			return cloner(obj, new DeepCloneState());
+		}
 
-        return cloner(obj, new DeepCloneState());
-    }
+		internal static object CloneClassInternal(object obj, DeepCloneState state)
+		{
+			if (obj == null) 
+				return null;
 
-    internal static object CloneClassInternal(object obj, DeepCloneState state)
-    {
-        if (obj == null)
-            return null;
+			var cloner = (Func<object, DeepCloneState, object>)DeepClonerCache.GetOrAddClass(obj.GetType(), t => GenerateCloner(t, true));
 
-        var cloner = (Func<object, DeepCloneState, object>)DeepClonerCache.GetOrAddClass(obj.GetType(), t => GenerateCloner(t, true));
+			// safe object
+			if (cloner == null) 
+				return obj;
 
-        // safe object
-        if (cloner == null)
-            return obj;
+			// loop
+			var knownRef = state.GetKnownRef(obj);
+			if (knownRef != null) 
+				return knownRef;
 
-        // loop
-        var knownRef = state.GetKnownRef(obj);
-        if (knownRef != null)
-            return knownRef;
+			return cloner(obj, state);
+		}
 
-        return cloner(obj, state);
-    }
+		private static T CloneStructInternal<T>(T obj, DeepCloneState state) // where T : struct
+		{
+			// no loops, no nulls, no inheritance
+			var cloner = GetClonerForValueType<T>();
 
-    private static T CloneStructInternal<T>(T obj, DeepCloneState state) // where T : struct
-    {
-        // no loops, no nulls, no inheritance
-        var cloner = GetClonerForValueType<T>();
+			// safe ojbect
+			if (cloner == null) 
+				return obj;
 
-        // safe ojbect
-        if (cloner == null)
-            return obj;
+			return cloner(obj, state);
+		}
 
-        return cloner(obj, state);
-    }
+		// when we can't use code generation, we can use these methods
+		internal static T[] Clone1DimArraySafeInternal<T>(T[] obj, DeepCloneState state)
+		{
+			var l = obj.Length;
+			var outArray = new T[l];
+			state.AddKnownRef(obj, outArray);
+			Array.Copy(obj, outArray, obj.Length);
+			return outArray;
+		}
 
-    // when we can't use code generation, we can use these methods
-    internal static T[] Clone1DimArraySafeInternal<T>(T[] obj, DeepCloneState state)
-    {
-        var l = obj.Length;
-        var outArray = new T[l];
-        state.AddKnownRef(obj, outArray);
-        Array.Copy(obj, outArray, obj.Length);
-        return outArray;
-    }
+		internal static T[] Clone1DimArrayStructInternal<T>(T[] obj, DeepCloneState state)
+		{
+			// not null from called method, but will check it anyway
+			if (obj == null) return null;
+			var l = obj.Length;
+			var outArray = new T[l];
+			state.AddKnownRef(obj, outArray);
+			var cloner = GetClonerForValueType<T>();
+			for (var i = 0; i < l; i++)
+				outArray[i] = cloner(obj[i], state);
 
-    internal static T[] Clone1DimArrayStructInternal<T>(T[] obj, DeepCloneState state)
-    {
-        // not null from called method, but will check it anyway
-        if (obj == null) return null;
-        var l = obj.Length;
-        var outArray = new T[l];
-        state.AddKnownRef(obj, outArray);
-        var cloner = GetClonerForValueType<T>();
-        for (var i = 0; i < l; i++)
-            outArray[i] = cloner(obj[i], state);
+			return outArray;
+		}
 
-        return outArray;
-    }
+		internal static T[] Clone1DimArrayClassInternal<T>(T[] obj, DeepCloneState state)
+		{
+			// not null from called method, but will check it anyway
+			if (obj == null) return null;
+			var l = obj.Length;
+			var outArray = new T[l];
+			state.AddKnownRef(obj, outArray);
+			for (var i = 0; i < l; i++)
+				outArray[i] = (T)CloneClassInternal(obj[i], state);
 
-    internal static T[] Clone1DimArrayClassInternal<T>(T[] obj, DeepCloneState state)
-    {
-        // not null from called method, but will check it anyway
-        if (obj == null) return null;
-        var l = obj.Length;
-        var outArray = new T[l];
-        state.AddKnownRef(obj, outArray);
-        for (var i = 0; i < l; i++)
-            outArray[i] = (T)CloneClassInternal(obj[i], state);
+			return outArray;
+		}
 
-        return outArray;
-    }
+		// relatively frequent case. specially handled
+		internal static T[,] Clone2DimArrayInternal<T>(T[,] obj, DeepCloneState state)
+		{
+			// not null from called method, but will check it anyway
+			if (obj == null) return null;
+			
+			// we cannot determine by type multidim arrays (one dimension is possible)
+			// so, will check for index here
+			var lb1 = obj.GetLowerBound(0);
+			var lb2 = obj.GetLowerBound(1);
+			if (lb1 != 0 || lb2 != 0)
+				return (T[,]) CloneAbstractArrayInternal(obj, state);
+			
+			var l1 = obj.GetLength(0);
+			var l2 = obj.GetLength(1);
+			var outArray = new T[l1, l2];
+			state.AddKnownRef(obj, outArray);
+			if (DeepClonerSafeTypes.CanReturnSameObject(typeof(T)))
+			{
+				Array.Copy(obj, outArray, obj.Length);
+				return outArray;
+			}
 
-    // relatively frequent case. specially handled
-    internal static T[,] Clone2DimArrayInternal<T>(T[,] obj, DeepCloneState state)
-    {
-        // not null from called method, but will check it anyway
-        if (obj == null) return null;
+			if (typeof(T).IsValueType())
+			{
+				var cloner = GetClonerForValueType<T>();
+				for (var i = 0; i < l1; i++)
+					for (var k = 0; k < l2; k++)
+						outArray[i, k] = cloner(obj[i, k], state);
+			}
+			else
+			{
+				for (var i = 0; i < l1; i++)
+					for (var k = 0; k < l2; k++)
+						outArray[i, k] = (T)CloneClassInternal(obj[i, k], state);
+			}
 
-        // we cannot determine by type multidim arrays (one dimension is possible)
-        // so, will check for index here
-        var lb1 = obj.GetLowerBound(0);
-        var lb2 = obj.GetLowerBound(1);
-        if (lb1 != 0 || lb2 != 0)
-            return (T[,])CloneAbstractArrayInternal(obj, state);
+			return outArray;
+		}
 
-        var l1 = obj.GetLength(0);
-        var l2 = obj.GetLength(1);
-        var outArray = new T[l1, l2];
-        state.AddKnownRef(obj, outArray);
-        if (DeepClonerSafeTypes.CanReturnSameObject(typeof(T)))
-        {
-            Array.Copy(obj, outArray, obj.Length);
-            return outArray;
-        }
+		// rare cases, very slow cloning. currently it's ok
+		internal static Array CloneAbstractArrayInternal(Array obj, DeepCloneState state)
+		{
+			// not null from called method, but will check it anyway
+			if (obj == null) return null;
+			var rank = obj.Rank;
 
-        if (typeof(T).IsValueType())
-        {
-            var cloner = GetClonerForValueType<T>();
-            for (var i = 0; i < l1; i++)
-                for (var k = 0; k < l2; k++)
-                    outArray[i, k] = cloner(obj[i, k], state);
-        }
-        else
-        {
-            for (var i = 0; i < l1; i++)
-                for (var k = 0; k < l2; k++)
-                    outArray[i, k] = (T)CloneClassInternal(obj[i, k], state);
-        }
+			var lengths = Enumerable.Range(0, rank).Select(obj.GetLength).ToArray();
 
-        return outArray;
-    }
+			var lowerBounds = Enumerable.Range(0, rank).Select(obj.GetLowerBound).ToArray();
+			var idxes = Enumerable.Range(0, rank).Select(obj.GetLowerBound).ToArray();
 
-    // rare cases, very slow cloning. currently it's ok
-    internal static Array CloneAbstractArrayInternal(Array obj, DeepCloneState state)
-    {
-        // not null from called method, but will check it anyway
-        if (obj == null) return null;
-        var rank = obj.Rank;
+			var elementType = obj.GetType().GetElementType();
+			var outArray = Array.CreateInstance(elementType, lengths, lowerBounds);
 
-        var lengths = Enumerable.Range(0, rank).Select(obj.GetLength).ToArray();
+			state.AddKnownRef(obj, outArray);
 
-        var lowerBounds = Enumerable.Range(0, rank).Select(obj.GetLowerBound).ToArray();
-        var idxes = Enumerable.Range(0, rank).Select(obj.GetLowerBound).ToArray();
+			// we're unable to set any value to this array, so, just return it
+			if (lengths.Any(x => x == 0))
+				return outArray;
 
-        var elementType = obj.GetType().GetElementType();
-        var outArray = Array.CreateInstance(elementType, lengths, lowerBounds);
+			if (DeepClonerSafeTypes.CanReturnSameObject(elementType))
+			{
+				Array.Copy(obj, outArray, obj.Length);
+				return outArray;
+			}
 
-        state.AddKnownRef(obj, outArray);
+			var ofs = rank - 1;
+			while (true)
+			{
+				outArray.SetValue(CloneClassInternal(obj.GetValue(idxes), state), idxes);
+				idxes[ofs]++;
 
-        // we're unable to set any value to this array, so, just return it
-        if (lengths.Any(x => x == 0))
-            return outArray;
+				if (idxes[ofs] >= lowerBounds[ofs] + lengths[ofs])
+				{
+					do
+					{
+						if (ofs == 0) return outArray;
+						idxes[ofs] = lowerBounds[ofs];
+						ofs--;
+						idxes[ofs]++;
+					} while (idxes[ofs] >= lowerBounds[ofs] + lengths[ofs]);
 
-        if (DeepClonerSafeTypes.CanReturnSameObject(elementType))
-        {
-            Array.Copy(obj, outArray, obj.Length);
-            return outArray;
-        }
+					ofs = rank - 1;
+				}
+			}
+		}
 
-        var ofs = rank - 1;
-        while (true)
-        {
-            outArray.SetValue(CloneClassInternal(obj.GetValue(idxes), state), idxes);
-            idxes[ofs]++;
+		internal static Func<T, DeepCloneState, T> GetClonerForValueType<T>()
+		{
+			return (Func<T, DeepCloneState, T>)DeepClonerCache.GetOrAddStructAsObject(typeof(T), t => GenerateCloner(t, false));
+		}
 
-            if (idxes[ofs] >= lowerBounds[ofs] + lengths[ofs])
-            {
-                do
-                {
-                    if (ofs == 0) return outArray;
-                    idxes[ofs] = lowerBounds[ofs];
-                    ofs--;
-                    idxes[ofs]++;
-                } while (idxes[ofs] >= lowerBounds[ofs] + lengths[ofs]);
-
-                ofs = rank - 1;
-            }
-        }
-    }
-
-    internal static Func<T, DeepCloneState, T> GetClonerForValueType<T>()
-    {
-        return (Func<T, DeepCloneState, T>)DeepClonerCache.GetOrAddStructAsObject(typeof(T), t => GenerateCloner(t, false));
-    }
-
-    private static object GenerateCloner(Type t, bool asObject)
-    {
-        if (DeepClonerSafeTypes.CanReturnSameObject(t) && (asObject && !t.IsValueType()))
-            return null;
+		private static object GenerateCloner(Type t, bool asObject)
+		{
+			if (DeepClonerSafeTypes.CanReturnSameObject(t) && (asObject && !t.IsValueType())) 
+				return null;
 
 #if !NETCORE
 			if (ShallowObjectCloner.IsSafeVariant()) return DeepClonerExprGenerator.GenerateClonerInternal(t, asObject);
 			else return DeepClonerMsilGenerator.GenerateClonerInternal(t, asObject);
 #else
-        return DeepClonerExprGenerator.GenerateClonerInternal(t, asObject);
+			return DeepClonerExprGenerator.GenerateClonerInternal(t, asObject);
 #endif
-    }
+		}
 
-    public static object CloneObjectTo(object objFrom, object objTo, bool isDeep)
-    {
-        if (objTo == null) return null;
+		public static object CloneObjectTo(object objFrom, object objTo, bool isDeep)
+		{
+			if (objTo == null) return null;
 
-        if (objFrom == null)
-            throw new ArgumentNullException("objFrom", "Cannot copy null object to another");
-        var type = objFrom.GetType();
-        if (!type.IsInstanceOfType(objTo))
-            throw new InvalidOperationException("From object should be derived from From object, but From object has type " + objFrom.GetType().FullName + " and to " + objTo.GetType().FullName);
-        if (objFrom is string)
-            throw new InvalidOperationException("It is forbidden to clone strings");
-        var cloner = (Func<object, object, DeepCloneState, object>)(isDeep
-            ? DeepClonerCache.GetOrAddDeepClassTo(type, t => ClonerToExprGenerator.GenerateClonerInternal(t, true))
-            : DeepClonerCache.GetOrAddShallowClassTo(type, t => ClonerToExprGenerator.GenerateClonerInternal(t, false)));
-        if (cloner == null) return objTo;
-        return cloner(objFrom, objTo, new DeepCloneState());
-    }
+			if (objFrom == null)
+				throw new ArgumentNullException("objFrom", "Cannot copy null object to another");
+			var type = objFrom.GetType();
+			if (!type.IsInstanceOfType(objTo))
+				throw new InvalidOperationException("From object should be derived from From object, but From object has type " + objFrom.GetType().FullName + " and to " + objTo.GetType().FullName);
+			if (objFrom is string)
+				throw new InvalidOperationException("It is forbidden to clone strings");
+			var cloner = (Func<object, object, DeepCloneState, object>)(isDeep
+				? DeepClonerCache.GetOrAddDeepClassTo(type, t => ClonerToExprGenerator.GenerateClonerInternal(t, true))
+				: DeepClonerCache.GetOrAddShallowClassTo(type, t => ClonerToExprGenerator.GenerateClonerInternal(t, false)));
+			if (cloner == null) return objTo;
+			return cloner(objFrom, objTo, new DeepCloneState());
+		}
+	}
 }
+
--- src/Foundatio/DeepCloner/Helpers/DeepClonerSafeTypes.cs
+++ src/Foundatio/DeepCloner/Helpers/DeepClonerSafeTypes.cs
@@ -1,49 +1,52 @@
-﻿#define NETCORE
+#define NETCORE
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
-namespace Foundatio.Force.DeepCloner.Helpers;
-
-/// <summary>
-/// Safe types are types, which can be copied without real cloning. e.g. simple structs or strings (it is immutable)
-/// </summary>
-internal static class DeepClonerSafeTypes
+namespace Foundatio.Force.DeepCloner.Helpers
 {
-    internal static readonly ConcurrentDictionary<Type, bool> KnownTypes = new();
+	/// <summary>
+	/// Safe types are types, which can be copied without real cloning. e.g. simple structs or strings (it is immutable)
+	/// </summary>
+	internal static class DeepClonerSafeTypes
+	{
+		internal static readonly ConcurrentDictionary<Type, bool> KnownTypes = new ConcurrentDictionary<Type, bool>();
 
-    static DeepClonerSafeTypes()
-    {
-        foreach (
-            var x in
-            new[]
-            {
-                typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long), typeof(ulong),
-                typeof(float), typeof(double), typeof(decimal), typeof(char), typeof(string), typeof(bool), typeof(DateTime),
-                typeof(IntPtr), typeof(UIntPtr), typeof(Guid),
-                // do not clone such native type
-                Type.GetType("System.RuntimeType"),
-                Type.GetType("System.RuntimeTypeHandle"),
+		static DeepClonerSafeTypes()
+		{
+			foreach (
+				var x in
+					new[]
+						{
+							typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long), typeof(ulong),
+							typeof(float), typeof(double), typeof(decimal), typeof(char), typeof(string), typeof(bool), typeof(DateTime),
+							typeof(IntPtr), typeof(UIntPtr), typeof(Guid),
+							// do not clone such native type
+							Type.GetType("System.RuntimeType"),
+							Type.GetType("System.RuntimeTypeHandle"),
+							StringComparer.Ordinal.GetType(),
+							StringComparer.CurrentCulture.GetType(), // CultureAwareComparer - can be same
 #if !NETCORE
 							typeof(DBNull)
 #endif
-            }) KnownTypes.TryAdd(x, true);
-    }
+						}) KnownTypes.TryAdd(x, true);
+		}
 
-    private static bool CanReturnSameType(Type type, HashSet<Type> processingTypes)
-    {
-        bool isSafe;
-        if (KnownTypes.TryGetValue(type, out isSafe))
-            return isSafe;
-
-        // enums are safe
-        // pointers (e.g. int*) are unsafe, but we cannot do anything with it except blind copy
-        if (type.IsEnum() || type.IsPointer)
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
+		private static bool CanReturnSameType(Type type, HashSet<Type> processingTypes)
+		{
+			bool isSafe;
+			if (KnownTypes.TryGetValue(type, out isSafe))
+				return isSafe;
+			
+			// enums are safe
+			// pointers (e.g. int*) are unsafe, but we cannot do anything with it except blind copy
+			if (type.IsEnum() || type.IsPointer)
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
 
 #if !NETCORE
 			// do not do anything with remoting. it is very dangerous to clone, bcs it relate to deep core of framework
@@ -62,11 +65,11 @@ internal static class DeepClonerSafeTypes
 
 			// catched by previous condition
 			/*if (type.FullName.StartsWith("System.Reflection.Emit") && type.Assembly == typeof(System.Reflection.Emit.OpCode).Assembly)
-            {
-                KnownTypes.TryAdd(type, true);
-                return true;
-            }*/
-
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}*/
+			
 			// this types are serious native resources, it is better not to clone it
 			if (type.IsSubclassOf(typeof(System.Runtime.ConstrainedExecution.CriticalFinalizerObject)))
 			{
@@ -81,117 +84,132 @@ internal static class DeepClonerSafeTypes
 				return true;
 			}
 #else
-        // do not copy db null
-        if (type.FullName.StartsWith("System.DBNull"))
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
+			// do not copy db null
+			if (type.FullName.StartsWith("System.DBNull"))
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
 
-        if (type.FullName.StartsWith("System.RuntimeType"))
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
+			if (type.FullName.StartsWith("System.RuntimeType"))
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
+			
+			if (type.FullName.StartsWith("System.Reflection.") && Equals(type.GetTypeInfo().Assembly, typeof(PropertyInfo).GetTypeInfo().Assembly))
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
 
-        if (type.FullName.StartsWith("System.Reflection.") && Equals(type.GetTypeInfo().Assembly, typeof(PropertyInfo).GetTypeInfo().Assembly))
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
+			if (type.IsSubclassOfTypeByName("CriticalFinalizerObject"))
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
+			
+			// better not to touch ms dependency injection
+			if (type.FullName.StartsWith("Microsoft.Extensions.DependencyInjection."))
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
 
-        if (type.IsSubclassOfTypeByName("CriticalFinalizerObject"))
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
-
-        // better not to touch ms dependency injection
-        if (type.FullName.StartsWith("Microsoft.Extensions.DependencyInjection."))
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
-
-        if (type.FullName == "Microsoft.EntityFrameworkCore.Internal.ConcurrencyDetector")
-        {
-            KnownTypes.TryAdd(type, true);
-            return true;
-        }
+			if (type.FullName == "Microsoft.EntityFrameworkCore.Internal.ConcurrencyDetector")
+			{
+				KnownTypes.TryAdd(type, true);
+				return true;
+			}
 #endif
+			// default comparers should not be cloned due possible comparison EqualityComparer<T>.Default == comparer
+			if (type.FullName.Contains("EqualityComparer"))
+			{
+				if (type.FullName.StartsWith("System.Collections.Generic.GenericEqualityComparer`")
+				    || type.FullName.StartsWith("System.Collections.Generic.ObjectEqualityComparer`")
+				    || type.FullName.StartsWith("System.Collections.Generic.EnumEqualityComparer`")
+				    || type.FullName.StartsWith("System.Collections.Generic.NullableEqualityComparer`")
+				    || type.FullName == "System.Collections.Generic.ByteEqualityComparer")
+				{
+					KnownTypes.TryAdd(type, true);
+					return true;
+				}
+			}
 
-        // classes are always unsafe (we should copy it fully to count references)
-        if (!type.IsValueType())
-        {
-            KnownTypes.TryAdd(type, false);
-            return false;
-        }
+			// classes are always unsafe (we should copy it fully to count references)
+			if (!type.IsValueType())
+			{
+				KnownTypes.TryAdd(type, false);
+				return false;
+			}
 
-        if (processingTypes == null)
-            processingTypes = new HashSet<Type>();
+			if (processingTypes == null)
+				processingTypes = new HashSet<Type>();
 
-        // structs cannot have a loops, but check it anyway
-        processingTypes.Add(type);
+			// structs cannot have a loops, but check it anyway
+			processingTypes.Add(type);
 
-        List<FieldInfo> fi = new List<FieldInfo>();
-        var tp = type;
-        do
-        {
-            fi.AddRange(tp.GetAllFields());
-            tp = tp.BaseType();
-        }
-        while (tp != null);
+			List<FieldInfo> fi = new List<FieldInfo>();
+			var tp = type;
+			do
+			{
+				fi.AddRange(tp.GetAllFields());
+				tp = tp.BaseType();
+			}
+			while (tp != null);
 
-        foreach (var fieldInfo in fi)
-        {
-            // type loop
-            var fieldType = fieldInfo.FieldType;
-            if (processingTypes.Contains(fieldType))
-                continue;
+			foreach (var fieldInfo in fi)
+			{
+				// type loop
+				var fieldType = fieldInfo.FieldType;
+				if (processingTypes.Contains(fieldType))
+					continue;
 
-            // not safe and not not safe. we need to go deeper
-            if (!CanReturnSameType(fieldType, processingTypes))
-            {
-                KnownTypes.TryAdd(type, false);
-                return false;
-            }
-        }
+				// not safe and not not safe. we need to go deeper
+				if (!CanReturnSameType(fieldType, processingTypes))
+				{
+					KnownTypes.TryAdd(type, false);
+					return false;
+				}
+			}
 
-        KnownTypes.TryAdd(type, true);
-        return true;
-    }
+			KnownTypes.TryAdd(type, true);
+			return true;
+		}
 
-    // not used anymore
-    /*/// <summary>
-    /// Classes with only safe fields are safe for ShallowClone (if they root objects for copying)
-    /// </summary>
-    private static bool CanCopyClassInShallow(Type type)
-    {
-        // do not do this anything for struct and arrays
-        if (!type.IsClass() || type.IsArray)
-        {
-            return false;
-        }
+		// not used anymore
+		/*/// <summary>
+		/// Classes with only safe fields are safe for ShallowClone (if they root objects for copying)
+		/// </summary>
+		private static bool CanCopyClassInShallow(Type type)
+		{
+			// do not do this anything for struct and arrays
+			if (!type.IsClass() || type.IsArray)
+			{
+				return false;
+			}
 
-        List<FieldInfo> fi = new List<FieldInfo>();
-        var tp = type;
-        do
-        {
-            fi.AddRange(tp.GetAllFields());
-            tp = tp.BaseType();
-        }
-        while (tp != null);
+			List<FieldInfo> fi = new List<FieldInfo>();
+			var tp = type;
+			do
+			{
+				fi.AddRange(tp.GetAllFields());
+				tp = tp.BaseType();
+			}
+			while (tp != null);
 
-        if (fi.Any(fieldInfo => !CanReturnSameType(fieldInfo.FieldType, null)))
-        {
-            return false;
-        }
+			if (fi.Any(fieldInfo => !CanReturnSameType(fieldInfo.FieldType, null)))
+			{
+				return false;
+			}
 
-        return true;
-    }*/
+			return true;
+		}*/
 
-    public static bool CanReturnSameObject(Type type)
-    {
-        return CanReturnSameType(type, null);
-    }
+		public static bool CanReturnSameObject(Type type)
+		{
+			return CanReturnSameType(type, null);
+		}
+	}
 }
+
