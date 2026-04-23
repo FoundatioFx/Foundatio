b4630460477c4e2fe5789413e8a83816993fdeca
Blake Niemyjski <bniemyjski@gmail.com>
2026-04-22 19:11:03 -0500

    revert: restore required keyword on LogEntry properties

--- src/Foundatio.Xunit/Logging/LogEntry.cs
+++ src/Foundatio.Xunit/Logging/LogEntry.cs
@@ -7,13 +7,13 @@ namespace Foundatio.Xunit;
 public class LogEntry
 {
     public DateTimeOffset Date { get; set; }
-    public string CategoryName { get; set; } = string.Empty;
+    public required string CategoryName { get; set; }
     public LogLevel LogLevel { get; set; }
-    public object[] Scopes { get; set; } = [];
+    public required object[] Scopes { get; set; }
     public EventId EventId { get; set; }
     public object? State { get; set; }
     public Exception? Exception { get; set; }
-    public Func<object?, Exception?, string> Formatter { get; set; } = static (s, _) => s?.ToString() ?? string.Empty;
+    public required Func<object?, Exception?, string> Formatter { get; set; }
     public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
 
     public string Message => Formatter(State, Exception);
